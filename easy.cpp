11ade3439c5167b82a5003f6d80c7793211d523d
dsoto30 <sotodaniel531@gmail.com>
2021-12-26 22:50:46 -0800

    Solved Buy & Sell Stock (easy)

---
+++ easy.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int lowestPrice = INT_MAX;
+        int profit = 0;
+        for (auto price: prices)
+        {
+            profit = std::max(profit, price - lowestPrice);
+            lowestPrice = std::min(price, lowestPrice);
+        }
+
+        return profit;
+    }
+};
